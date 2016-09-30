49ae85009814dd42747c753998ac335ae0332e65
Anant Joshi <anantjoshi1324@gmail.com>
2016-10-01 01:42:21 +0530

    Fix alarm not playing because of lack of flag

--- main/java/com/kjsce/hackathon/medicinereminder/alarm/AlarmBroadcastReciever.java
+++ main/java/com/kjsce/hackathon/medicinereminder/alarm/AlarmBroadcastReciever.java
@@ -13,6 +13,7 @@ public class AlarmBroadcastReciever extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent launchIntent = new Intent(context, ReminderActivity.class);
+        launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(launchIntent);
     }
 }
